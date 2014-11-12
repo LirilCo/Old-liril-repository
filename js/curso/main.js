6ffd49954765037d1a39ea8e06024962f56a30bf
Luis Edº Gallego García. <luis12345671@live.com>
2014-11-12 00:15:34 -0500

    Shit's getting real

---
+++ js/curso/main.js
@@ -0,0 +1,112 @@
+$(document).on("ready",function(){
+	$("#index-trigger").on("click",function(){
+		$("#index-menu").toggleClass("open");
+		$("#index-text").toggleClass("open");
+		$("#index-trigger .index-arrow").toggleClass("open");
+		$(".indexMenuItem").toggleClass("fadeInDown");
+		$(".indexMenuItem").toggleClass("fadeOutUp");
+		$(".indexMenuItem").toggleClass("open");
+
+	});
+	var wasPlaying = false;
+	$("#dots").on("click",function(){
+		$("#Store").toggleClass("open");
+		$("#index-trigger").children().toggleClass("openedStore");
+		$("#right-menu #dots").children().toggleClass("openedStore");
+		$("#right-menu").children().toggleClass("openedStore");
+		if(!player.paused() && $("#Store").hasClass("open")){
+			player.pause();
+			wasPlaying = true;
+		}
+		if(player.paused() && !$("#Store").hasClass("open") && wasPlaying === true){
+			player.play();
+			wasPlaying = false;
+		}
+
+	});
+
+	setInterval(function(){
+        if(player.paused()){
+            $("#play").removeClass("playing")
+        }else{
+            $("#play").addClass("playing")
+		}
+		$("#example_video_1").height($(window).height() - $("header").height() - 12);
+		$("#Store #actualStore").height($(window).height() - $("header").height() - 21);
+		$("#Store #actualStore #storeSection").width($(window).width() - $("#Store #actualStore #storeList").width() - 87);
+		$("#Store #actualStore #storeSection").height($("#Store #actualStore").height() - 80);
+
+
+		if($("#profileSettings").hasClass("open")){
+			$('.knob').trigger(
+        		'configure',
+        		{   
+        		    "fgColor":"#2ecc71"
+        		}
+    		);
+		}if(!$("#profileSettings").hasClass("open") && $("#Store").hasClass("open")){
+			$('.knob').trigger(
+        		'configure',
+        		{   
+        		    "fgColor":"#fff"
+        		}
+    		);
+		}
+        if(!$("#profileSettings").hasClass("open") && !$("#Store").hasClass("open")){
+			$('.knob').trigger(
+        		'configure',
+        		{   
+        		    "fgColor":"#2ecc71"
+        		}
+    		);
+		}
+        
+	}, 10);
+
+	$("#profileTrigger").on("click",function(){
+		$("#right-menu .index-arrow").toggleClass("open");
+		$("#profileSettings").toggleClass("open");
+		
+	});
+    $("#play").on("click",function(){
+        if(player.paused()){
+		player.play()
+        }else{
+        player.pause();
+		}
+	});
+	
+	jump("#start",0);
+	jump("#second",10);
+	jump("#third",20);
+	jump("#fourth",35);
+	$(".vjs-play-control.vjs-control").css({
+		"display": "none"
+	});
+	$(".vjs-current-time.vjs-time-controls.vjs-control").css({
+		"display": "none"
+	});
+	$(".vjs-time-divider").css({
+		"display": "none"
+	});
+	$(".vjs-duration-display").css({
+		"display": "none"
+	});
+	$(".vjs-mute-control.vjs-control").css({
+		"display": "none"
+	});
+$(".vjs-volume-control.vjs-control").css({
+		"display": "none"
+	});
+$(".vjs-captions-button.vjs-menu-button.vjs-control ").css({
+		"display": "none"
+	});
+
+
+});
+
+function jump(s,t){
+		$(s).on("click",function(){
+		player.currentTime(t);
+	});
+}
--- js/index/main.js
+++ js/index/main.js
@@ -14,32 +14,23 @@ $(document).on("ready",function(){
 		$("#index-trigger").children().toggleClass("openedStore");
 		$("#right-menu #dots").children().toggleClass("openedStore");
 		$("#right-menu").children().toggleClass("openedStore");
-		if(!player.paused() && $("#Store").hasClass("open")){
-			player.pause();
-			wasPlaying = true;
-		}
-		if(player.paused() && !$("#Store").hasClass("open") && wasPlaying === true){
-			player.play();
-			wasPlaying = false;
-		}
-
 	});
 
 	setInterval(function(){
+
 		$("#example_video_1").height($(window).height() - $("header").height() - 12);
 		$("#Store #actualStore").height($(window).height() - $("header").height() - 21);
 		$("#Store #actualStore #storeSection").width($(window).width() - $("#Store #actualStore #storeList").width() - 87);
 		$("#Store #actualStore #storeSection").height($("#Store #actualStore").height() - 80);
-
-
-		if($("#profileSettings").hasClass("open")){
+        
+        if($("#profileSettings").hasClass("open")){
 			$('.knob').trigger(
         		'configure',
         		{   
         		    "fgColor":"#2ecc71"
         		}
     		);
-		}if(!$("#profileSettings").hasClass("open") && $("#Store").hasClass("open")){
+		}if($("#Store").hasClass("open")){
 			$('.knob').trigger(
         		'configure',
         		{   
@@ -55,7 +46,14 @@ $(document).on("ready",function(){
         		}
     		);
 		}
-        
+        if($("#profileSettings").hasClass("open") && $("#Store").hasClass("open")){
+			$('.knob').trigger(
+        		'configure',
+        		{   
+        		    "fgColor":"#2ecc71"
+        		}
+    		);
+		}
 	}, 10);
 
 	$("#profileTrigger").on("click",function(){
@@ -64,54 +62,6 @@ $(document).on("ready",function(){
 		
 	});
 	
-	jump("#start",0);
-	jump("#second",10);
-	jump("#third",20);
-	jump("#fourth",35);
-	$(".vjs-play-control.vjs-control").css({
-		"position": "fixed",
-		"top": "-3px",
-		"left": "230px"
-	});
-	$(".vjs-current-time.vjs-time-controls.vjs-control").css({
-		"position": "fixed",
-		"top": "-10px",
-		"left": "270px",
-		"font-size":"20px"
-	});
-	$(".vjs-time-divider").css({
-		"position": "fixed",
-		"top": "-10px",
-		"left": "335px",
-		"font-size":"20px"
-	});
-	$(".vjs-duration-display").css({
-		"position": "fixed",
-		"top": "5px",
-		"left": "345px",
-		"font-size":"20px"
-	});
-	$(".vjs-mute-control.vjs-control").css({
-		"position": "fixed",
-		"top": "5px",
-		"left": "400px"
-	});
-$(".vjs-volume-control.vjs-control").css({
-		"position": "fixed",
-		"top": "7px",
-		"left": "435px"
-	});
-$(".vjs-captions-button.vjs-menu-button.vjs-control ").css({
-		"position": "fixed",
-		"top": "6px",
-		"left": "500px"
-	});
-
-
+	
 });
 
-function jump(s,t){
-		$(s).on("click",function(){
-		player.currentTime(t);
-	});
-}
